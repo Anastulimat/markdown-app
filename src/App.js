a4920b1b3cbb0f60c9bbb0799e875617d5077760
Anas TULIMAT <anas.tulimat@outlook.fr>
2021-03-03 09:53:16 +0100

    feat: convert text to markdonw

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { sampleText } from "./sampleText";
+import marked from 'marked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: sampleText
+    }
+  }
+
+  handleChange = (event) => {
+    const text = event.target.value;
+    this.setState({
+      text : text
+    })
+  }
+
+  renderText = (text) => {
+    return marked(text, {sanitize: true})
+  }
+
+  render() {
+    return (
+        <div className="container mt-4">
+          <div className="row">
+            <div className="col-sm-6">
+              <h1>MarkDown</h1>
+              <textarea
+                  onChange={this.handleChange}
+                  value={this.state.text}
+                  name="text"
+                  id="text"
+                  cols="30"
+                  rows="30"
+                  className='form-control'
+              >
+          </textarea>
+            </div>
+
+            <div className="col-sm-6">
+              <h1>Résultat</h1>
+              <div  dangerouslySetInnerHTML={{ __html: this.renderText(this.state.text) }} />
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
